1f2f77e51a5caa6101851fa77b01f8c874124927
Ihame-b <ihamegrbt@gmail.com>
2022-12-06 17:53:15 +0200

    add admin

---
+++ js/app.js
@@ -0,0 +1,15 @@
+function showData() {
+ document.getElementById('displayname').innerHTML = 
+ document.getElementById("name").value;
+              
+ document.getElementById('displaydescription').innerHTML = 
+ document.getElementById("dicription").value;  
+
+ document.getElementById('displaydate').innerHTML = 
+ document.getElementById("date1").value;  
+
+ document.getElementById('displaypic').innerHTML = 
+ document.getElementById("myFile").value;  
+
+}
+
